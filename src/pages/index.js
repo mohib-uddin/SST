740acade2e95b9c57bb406da174e2a3f4cabdd4c
itzzmohib31 <mohibplayz31@gmail.com>
2023-02-27 00:48:49 +0500

    new update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/home.module.css'
 const inter = Inter({ subsets: ['latin'] })
 import Sidebar from './Components/Sidebar'
-import {signIn} from "next-auth/react";
 import { useEffect } from 'react'
 import Router from 'next/router'
 import { useSession } from 'next-auth/react'
