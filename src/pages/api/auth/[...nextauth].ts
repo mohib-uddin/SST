493b13e87beac94758d748a222fc3d44472f1403
itzzmohib31 <mohibplayz31@gmail.com>
2023-02-26 23:52:21 +0500

    hello

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { NextAuthOptions } from "next-auth";
-import User from "models/User";
 
 
 const authOptions:NextAuthOptions={
